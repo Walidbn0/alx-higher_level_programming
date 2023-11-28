3704a80de9e751f8d058622935d4d599db1da085
Walidbn0 <waliidbenhammii@gmail.com>
2023-11-28 14:38:55 -0800

    python7

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # Author - Tolulope Fakunle
 for lettre in range (97, 123):
-    if chr(lettre) != 'e' and chr(lettre) != 'e':
+    if chr(lettre) != 'e' and chr(lettre) != 'q':
         print("{}".format(chr(lettre)), end="")
