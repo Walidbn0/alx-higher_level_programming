a44a3a80d6077ca75ac966270af51cc1cc865a8a
Walidbn0 <waliidbenhammii@gmail.com>
2023-12-04 14:12:41 -0800

    data struct py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list):
-        my_list[idx] = element
-    return(my_list)
+
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return
+    return my_list[idx]
