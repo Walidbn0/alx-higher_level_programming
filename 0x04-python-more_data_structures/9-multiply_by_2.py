c66e5a81bf6591b45cf6b6668a4c874575d51ca0
Walidbn0 <waliidbenhammii@gmail.com>
2023-12-06 16:12:47 -0800

    data struct

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def multiply_by_2(my_dict):
-    print{key : value * 2 for key, value in my_dict.item()}
+def multiply_by_2(a_dictionary):
+    new_dir = a_dictionary.copy()
+    list_keys = list(new_dir.keys())
+
+    for i in list_keys:
+        new_dir[i] *= 2
+
+    return (new_dir)
